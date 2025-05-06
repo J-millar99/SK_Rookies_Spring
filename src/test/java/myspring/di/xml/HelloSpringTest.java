c02af003932de5a2c18d8fb6cbc342664522ba59
J-millar99 <wlwogus33@gmail.com>
2025-05-06 18:21:47 +0900

    HelloSpringTest update

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -1,8 +1,12 @@
 package myspring.di.xml;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -13,8 +17,19 @@ public class HelloSpringTest {
 	@Autowired
 	Hello hello;
 	
+	@Autowired
+	Printer strPrinter;
+	
+	@Autowired
+	@Qualifier("strPrinter")
+	Printer printer;
+	
 	@Test
 	void helloSpringBean() {
-		
+		assertEquals("Hello 스프링", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 스프링", strPrinter.toString());
+		System.out.println(strPrinter.getClass().getName());
+		assertEquals("Hello 스프링", printer.toString());
 	}
 }
