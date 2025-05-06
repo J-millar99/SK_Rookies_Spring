e895ebafc4a83b9d4d17b4f5ad8f485a3a20c781
J-millar99 <wlwogus33@gmail.com>
2025-05-06 18:00:20 +0900

    junit update

--- src/test/java/myspring/di/xml/HelloJunitTest.java
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -1,6 +1,7 @@
 package myspring.di.xml;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 // Assertions에 포함된 모든 static method를 import 한다.
 import static org.junit.jupiter.api.Assertions.assertSame;
 
@@ -20,6 +21,11 @@ public class HelloJunitTest {
 		System.out.println(helloById == helloByType);
 		assertSame(helloById, helloByType);
 		
+		assertEquals("Hello 스프링", helloById.sayHello());
 		
+		helloById.print();
+		
+		// Container 객체가 생성한 StringPrinter 스프링빈을 요청하기
+		Printer printer = context.getBean("strPrinter", Printer.class);
 	}
 }
