f673ea19d0ec36e1356b0bffb4e1d3ff4bd757e1
J-millar99 <wlwogus33@gmail.com>
2025-05-06 17:21:33 +0900

    junit update

--- src/test/java/myspring/di/xml/HelloJunitTest.java
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -1,10 +1,13 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloJunitTest {
 	@Test
 	void helloBean() {
-		System.out.println("Hello Junit");
+		// 1. Container 객체 생성
+		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-di.xml");
 	}
 }
