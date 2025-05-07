2cc9493940e78407b321425483aef12ccca28c46
J-millar99 <wlwogus33@gmail.com>
2025-05-07 20:12:07 +0900

    HelloConfig update

--- src/main/java/myspring/di/xml/config/HelloConfig.java
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -40,6 +40,8 @@ public class HelloConfig {
 	
 	@Bean
 	public List<String> namesList() {
-		return Arrays.asList("Java", "Spring", "Kotlin");
+		String names = env.getProperty("names.list.of.strings");
+		String[] strArray = names.split(",");
+		return Arrays.asList(strArray);
 	}
 }
