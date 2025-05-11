6b724b1b75fbf2f42c2be58f6411a8bc5b556b1e
J-millar99 <wlwogus33@gmail.com>
2025-05-12 00:10:21 +0900

    exercise 1-3

--- src/main/java/mylab/notification/di/annot/SmsNotificationService.java
+++ src/main/java/mylab/notification/di/annot/SmsNotificationService.java
@@ -1,4 +1,19 @@
 package mylab.notification.di.annot;
 
-public class SmsNotificationService {
+public class SmsNotificationService implements NotificationService {
+    private String provider;
+
+    public SmsNotificationService(String provider) {
+        this.provider = provider;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    @Override
+    public void sendNotification(String message) {
+        System.out.println("provider = " + provider);
+        System.out.println("message = " + message);
+    }
 }
--- src/test/java/mylab/notification/di/annot/NotificationConfigTest.java
+++ src/test/java/mylab/notification/di/annot/NotificationConfigTest.java
@@ -1,4 +1,37 @@
 package mylab.notification.di.annot;
 
+import mylab.notification.di.annot.config.NotificationConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = NotificationConfig.class)
 public class NotificationConfigTest {
+    @Autowired
+    private NotificationManager notificationManager;
+
+    @Test
+    public void testNotification() {
+        // 정상적인 NotificationManager 생성 확인
+        assertNotNull(notificationManager);
+
+        // email 서비스 검증
+        NotificationService email = notificationManager.getEmailService();
+        assertTrue(email instanceof EmailNotificationService);
+        assertEquals("smtp.gmail.com", ((EmailNotificationService) email).getSmtpServer());
+        assertEquals(587, ((EmailNotificationService)email).getPort());
+
+        // sms 서비스 검증
+        NotificationService sms = notificationManager.getSmsService();
+        assertTrue(sms instanceof SmsNotificationService);
+        assertEquals("SKT", ((SmsNotificationService)email).getProvider());
+
+        email.sendNotification("emailNotificationService Test");
+        sms.sendNotification("smsNotificationService Test");
+    }
 }
